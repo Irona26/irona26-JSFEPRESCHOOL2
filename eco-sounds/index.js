ad04aa120d40cccc20ba795c1b3de9f803141684
Irona May <irina_kulinich@mail.ru>
2022-02-04 21:21:00 +0300

    feat: sounds

---
+++ eco-sounds/index.js
@@ -0,0 +1,47 @@
+
+/* смена кнопки и звука */
+
+const button = document.querySelector('button');
+const playBtn = document.querySelector('.play-btn');
+const pauseBtn = document.querySelector('pause');
+const audio = new Audio();
+let isPlay = false;
+
+
+function playAudio() {
+ if(!isPlay) {
+    audio.currentTime = 0;
+    audio.play();
+    isPlay = true;
+} else {
+  audio.pause();
+  isPlay = false;
+}
+}
+
+/*  смена картинок и мелодий  */
+
+const navBtns = document.querySelectorAll('.nav-link');
+const navBtn = document.querySelector('.nav-container');
+const videoImg = document.querySelectorAll('.img-video');
+
+function changeImage(event) {
+  const bird = event.target.dataset.bird;
+    if(event.target.classList.contains('nav-link')) {
+      videoImg.forEach((img) => img.src = `assets/img/${bird}.jpg`);
+      audio.src = `assets/audio/${bird}.mp3`;
+      navBtns.forEach((btn) => btn.classList.remove('active'));
+      event.target.classList.add('active'); 
+     if (isPlay == true) {
+       audio.play();
+     }
+    }
+}
+navBtn.addEventListener('click', changeImage);
+
+function toggleBtn() {
+  button.classList.toggle('pause');
+  audio.src = `assets/audio/forest.mp3`;
+}
+playBtn.addEventListener('click', toggleBtn);
+playBtn.addEventListener('click', playAudio);
